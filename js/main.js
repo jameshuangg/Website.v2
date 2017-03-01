6443a9f1183e995d8cf5e24c871f9d4942cd3e2a
James <james.sohuang@hotmail.com>
2017-02-28 23:31:47 -0500

    Font update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,32 @@
 	
 		angular.module("myWebsite", [])
 		.controller("mainController", function() {
-			
+			this.employment = [
+				{
+					employer: "nanoPay",
+					title: "Quality Assurance Engineer / Web Developer",
+					duration: "January 2017 - April 2017",
+					description: "Utilized Selenium and Calabash to integrate clean and efficient automation testing for mobile and web applications. Utilized various technologies and frameworks to develop web based applicaions.",
+					skills: [
+						{
+							name: "calabash",
+							level: "70%"
+						},
+						{
+							name: "selenium",
+							level: "50%"
+						},
+						{
+							name: "javascript",
+							level: "70%"
+						},
+						{
+							name: "angularJS",
+							level: "70%"
+						}
+					],
+					
+				}
+			];
 		});
 })();
